2b36a6fa4f32b1d11cc095afc3e54117e798ce58
oana-rancov <oanamariarancov@yahoo.com>
2021-07-19 18:37:45 +0300

    App Icon and SplashScreen

--- app/src/main/java/com/example/concerttickets/SplashActivity.java
+++ app/src/main/java/com/example/concerttickets/SplashActivity.java
@@ -6,18 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
     private static int SPLASH_TIMEOUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
